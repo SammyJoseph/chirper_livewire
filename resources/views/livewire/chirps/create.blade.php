28ff04fdee41bb8d1ee7265ce1c9733c9d8800e0
SammyJoseph <sam.tab.paz@gmail.com>
2023-09-11 15:38:39 -0500

    listando chirps

--- resources/views/livewire/chirps/create.blade.php
+++ resources/views/livewire/chirps/create.blade.php
@@ -4,23 +4,26 @@ use Livewire\Attributes\Rule;
 use Livewire\Volt\Component;
 
 new class extends Component {
-    #[Rule('required|string|min:3|max:255')] // aquí se define la regla de validate() en livewire
-
+    #[Rule('required|string|min:3|max:255')] // aquí se define la regla de validate() para $message
     public $message = '';
 
     public function store(): void
     {
         $validated = $this->validate();
+        // dd($validated);
 
         auth()->user()->chirps()->create($validated);
 
         $this->message = '';
+
+        $this->dispatch('chirp-created'); // enviar un evento cada vez que se crea un chirp (se recibe desde list de livewire)
     }
 }; ?>
 
 <div>
     <form wire:submit="store">
         <textarea
+            required
             wire:model="message"
             placeholder="{{ __('What\'s on your mind?') }}"
             class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
